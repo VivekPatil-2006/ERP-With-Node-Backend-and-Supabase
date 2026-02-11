e9746f6e20916249bdaa6413d58d4a3b75c12bf3
VivekPatil-2006 <vivek.j.patil2006@gmail.com>
2026-02-11 17:27:20 +0530

    Client Updated

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,8 +4,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class ApiService {
   // 🔁 Change this when deploying
-  //static const String baseUrl = "https://j3rpjd6r-8000.inc1.devtunnels.ms/api";
-  static const String baseUrl = "http://192.168.1.5:5000/api";
+  static const String baseUrl = "https://j3rpjd6r-8000.inc1.devtunnels.ms/api";
+  //static const String baseUrl = "http://192.168.1.5:5000/api";
 
   /* =======================================================
      🔐 Get Firebase ID Token (PRIVATE ROUTES)
